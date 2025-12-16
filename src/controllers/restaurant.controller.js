cf8365f949c361efcb4afbbfdd657e0c7883fa92
kailash kumawat <kumawatkai369@gmail.com>
2025-12-16 21:17:54 +0530

    Add unique constraint to Restaurant model and implement restaurant creation and update logic with validation

---
+++ src/controllers/restaurant.controller.js
@@ -0,0 +1,71 @@
+import { ApiError, asyncHandler, ApiResponse } from "../utils/ApiError.js";
+import * as restaurantServices from "../services/restaurant.service.js";
+
+// i'll available the option to user register your restra, team contacted ui,
+// give restra info form and call this controller.
+
+export const createRestaurant = asyncHandler(async (req, res) => {
+  //get restra info from owner
+  const { name, city, address_line, pincode, contact } = req.body;
+  //check field not empty
+  if (
+    [name, city, address_line, pincode, contact].some(
+      (field) => !field || field.trim() == "",
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  //send info to service
+  const createdRestaurant = await restaurantServices.createRestaurant({
+    name,
+    city,
+    address_line,
+    pincode,
+    contact,
+  });
+  //res
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        createdRestaurant,
+        "Restaurant registered successfully",
+      ),
+    );
+});
+
+export const updateRestaurant = asyncHandler(async (req, res) => {
+  // get info from user to update
+  const { name, city, address_line, pincode, contact } = req.body;
+  const restaurantId = req.restaurant.id;
+  // check atleast have one field
+  if (
+    [name, city, address_line, pincode, contact].every(
+      (field) => !field || field.trim() === "",
+    )
+  ) {
+    throw new ApiError(400, "At least one field is required");
+  }
+  // send to controller
+  const updatedRestaurant = await restaurantServices.updateRestaurant(
+    {
+      name,
+      city,
+      address_line,
+      pincode,
+      contact,
+    },
+    restaurantId,
+  );
+  // return res
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        updatedRestaurant,
+        "Restaurant updated successfully",
+      ),
+    );
+});
--- src/services/restaurant.service.js
+++ src/services/restaurant.service.js
@@ -1,4 +1,95 @@
+import { ApiError } from "../utils/ApiError.js";
+import prisma from "../db/index.js";
+
 // create restaurant
+export const createRestaurant = async ({
+  name,
+  city,
+  address_line,
+  pincode,
+  contact,
+}) => {
+  // check restra existed or not
+  // check using all of info see gpt
+  const existedRestaurant = await prisma.restaurant.findFirst({
+    where: {
+      name,
+      city,
+      address_line,
+      pincode,
+    },
+    select: {
+      id: true,
+      name: true,
+    },
+  });
+
+  if (existedRestaurant) {
+    throw new ApiError(409, "Restaurant already exists");
+  }
+  // return create why try catch -> handling 2 same restaurant creation at same time.
+  try {
+    return await prisma.restaurant.create({
+      data: {
+        name,
+        city,
+        address_line,
+        pincode,
+        contact,
+      },
+      select: {
+        name: true,
+        city: true,
+        address_line: true,
+        pincode: true,
+        contact: true,
+      },
+    });
+  } catch (error) {
+    if (error.code === "P2002") {
+      throw new ApiError(409, "Restaurant already exists");
+    }
+    throw error;
+  }
+};
+
 // update
+export const updateRestaurant = async (
+  { name, city, address_line, pincode, contact },
+  restaurantId,
+) => {
+  const isRestaurantExist = await prisma.restaurant.findUnique({
+    where: {
+      id: restaurantId,
+    },
+  });
+
+  if (isRestaurantExist) {
+    throw new ApiError(400, "Restaurant not found");
+  }
+
+  const updateData = {
+    ...(name && { name }),
+    ...(city && { city }),
+    ...(address_line && { address_line }),
+    ...(pincode && { pincode }),
+    ...(contact && { contact }),
+  };
+
+  return await prisma.restaurant.update({
+    where: {
+      id: restaurantId,
+    },
+    data: updateData,
+    select: {
+      id: true,
+      name: true,
+      city: true,
+      address_line: true,
+      pincode: true,
+      contact: true,
+    },
+  });
+};
 // delete
 // get
