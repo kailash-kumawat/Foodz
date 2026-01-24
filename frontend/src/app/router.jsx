c9b253d3ce5a26e9abf2ebd12d7194a18ce87631
kailash kumawat <kumawatkai369@gmail.com>
2026-01-24 21:57:19 +0530

    feat: add Signup component and update router for signup route

--- frontend/src/app/router.jsx
+++ frontend/src/app/router.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import AuthLayout from "../layouts/AuthLayout";
-import Login from "../features/auth/Login";
+import { Login, Signup } from "../features/auth/index.js";
 
 function router() {
   return (
     <Routes>
       <Route element={<AuthLayout />}>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
       </Route>
     </Routes>
   );
---
+++ frontend/src/features/auth/Signup.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Button, Input } from "../../components/index.js";
+
+function Signup() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isValid },
+  } = useForm({ mode: "onChange" });
+
+  const onSubmit = (data) => {
+    console.log("Signup data: ", data);
+  };
+
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          type="text"
+          label="Name"
+          {...register("name", { required: "Name is required" })}
+          error={errors.name?.message}
+        />
+        <Input
+          type="email"
+          label="Email address"
+          {...register("email", { required: "Email is required" })}
+          error={errors.email?.message}
+        />
+        <Input
+          type="tel"
+          label="Phone no"
+          {...register("contact", { required: "Contact is required" })}
+          error={errors.contact?.message}
+        />
+        <Input
+          type="password"
+          label="Password"
+          {...register("password", { required: "Password is required" })}
+          error={errors.password?.message}
+        />
+        <Button type="submit" loading={isSubmitting} disabled={!isValid}>
+          Signup
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
