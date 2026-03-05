94096b47bd671d069d7aba3d344eefd64a9c249b
kailash kumawat <kumawatkai369@gmail.com>
2026-03-05 12:26:26 +0530

    feat: add BackButton component and integrate it into SingleDish for navigation

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ChevronLeft } from "lucide-react";
+
+function BackButton() {
+  return (
+    // <div className="">
+    <button>
+      <ChevronLeft className="text-black" />
+    </button>
+    // </div>
+  );
+}
+
+export default BackButton;
--- frontend/src/components/SingleDish.jsx
+++ frontend/src/components/SingleDish.jsx
@@ -3,44 +3,50 @@ import { Button } from "./index.js";
 import { IndianRupee } from "lucide-react";
 import { useParams } from "react-router-dom";
 import { foods } from "../data/foods.data.js";
+import BackButton from "./BackButton.jsx";
 
 function SingleDish() {
   const { id } = useParams();
   const filteredFood = foods.find((food) => food.id === Number(id));
 
   return (
-    <div className="p-6 flex  flex-col items-center justify-items-center">
-      {/* Dish Image */}
-      <img
-        src={filteredFood.image}
-        alt={filteredFood.name}
-        className="
+    <>
+      <div className="p-6 flex flex-col items-center justify-items-center">
+        {/* Back button */}
+        <div className="mr-auto">
+          <BackButton />
+        </div>
+        {/* Dish Image */}
+        <img
+          src={filteredFood.image}
+          alt={filteredFood.name}
+          className="
         w-[240px] h-[240px]
-          rounded-full
-          object-cover
-          mb-4
-          shadow-black/50
-          shadow-2xl
+        rounded-full
+        object-cover
+        mb-4
+        shadow-black/50
+        shadow-2xl
         "
-      />
+        />
 
-      {/* Dish Name */}
-      <h6
-        className="
-          text-2xl
-          font-semibold
-          text-center
-          w-full
-          leading-tight
-          mt-3
+        {/* Dish Name */}
+        <h6
+          className="
+        text-2xl
+        font-semibold
+        text-center
+        w-full
+        leading-tight
+        mt-3
         "
-      >
-        {filteredFood.name}
-      </h6>
+        >
+          {filteredFood.name}
+        </h6>
 
-      {/* Dish Price */}
-      <span
-        className="
+        {/* Dish Price */}
+        <span
+          className="
           text-xl
           font-bold
           text-[#FA4A0C]
@@ -48,31 +54,29 @@ function SingleDish() {
           items-center
           pr-2
         "
-      >
-        <IndianRupee className="w-4" />
-        {filteredFood.price}
-      </span>
+        >
+          <IndianRupee className="w-4" />
+          {filteredFood.price}
+        </span>
 
-      {/* Description */}
-      <div className="p-6">
-        <h4 className="font-bold">Description</h4>
-        <p className="text-black opacity-50">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur et
-          maiores laboriosam ut dicta quos non repudiandae iure, nemo adipisci
-          eaque est commodi, veniam obcaecati vitae incidunt dolorem
-          voluptatibus quod.
-        </p>
+        {/* Description */}
+        <div className="p-6">
+          <h4 className="font-bold">Description</h4>
+          <p className="text-black opacity-50">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur et
+            maiores laboriosam ut dicta quos non repudiandae iure, nemo adipisci
+            eaque est commodi, veniam obcaecati vitae incidunt dolorem
+            voluptatibus quod.
+          </p>
+        </div>
+
+        {/* Add to cart button component */}
+        <Button type="submit" className="text-[#F6F6F9]">
+          Add to cart
+        </Button>
       </div>
-
-      {/* Add to cart button component */}
-      <Button type="submit" className="text-[#F6F6F9]">
-        Add to cart
-      </Button>
-    </div>
+    </>
   );
 }
 
 export default SingleDish;
-
-// next: here make it fully functional and check width of description when screen is expending it expend too.
-// get name, img, price, description
