e7a94e03081586abe1d39050945a5db07a341db1
kailash kumawat <kumawatkai369@gmail.com>
2026-04-12 13:23:34 +0530

    feat: update Checkout component to include navigation for adding items; enhance item display and price calculations

--- frontend/src/features/checkout/Checkout.jsx
+++ frontend/src/features/checkout/Checkout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BackButton, Button } from "../../components/index.js";
 import { useCartStore } from "../../store/cart.store.js";
 import { UtensilsCrossed, IndianRupee } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PriceRow from "./PriceRow";
 
 function Checkout() {
@@ -50,95 +50,73 @@ function Checkout() {
         <div className="w-5/6 mx-auto mt-12 lg:w-1/2">
           <div className="flex justify-between w-full">
             <p className="text-lg font-semibold">Order details</p>
-            <p className="text-lg text-[#F47B0A]">Add items +</p>
+            <Link to={"/"} className="text-lg text-[#F47B0A] cursor-pointer">
+              Add items +
+            </Link>
           </div>
 
           <div className="bg-white rounded-[20px] p-6 mt-3 flex flex-col gap-2">
-            {cartItems.length > 0
-              ? cartItems.map((item) => (
-                  <>
-                    <div
-                      key={item.id}
-                      className="flex items-center gap-2 text-lg"
-                    >
+            {cartItems.length > 0 ? (
+              <>
+                {cartItems.map((item) => (
+                  <div key={item.id}>
+                    <div className="flex items-center gap-2 text-lg">
                       <UtensilsCrossed size={15} />
                       <p>{item.name}</p>
-                      <p>{`x${item.quantity}`}</p>
+                      <p className="text-gray-500">{`x${item.quantity}`}</p>
                       <span className="ml-auto flex items-center">
                         <IndianRupee size={10} />
                         <p>{`${item.price * item.quantity}`}</p>
                       </span>
                     </div>
                     <hr className="border-t border-black/20" />
-                  </>
-                ))
-              : "Add Items to cart"}
+                  </div>
+                ))}
+                <PriceRow
+                  name={"Subtotal"}
+                  amount={totalAmount}
+                  className="flex items-center justify-between text-lg font-extralight italic"
+                />
 
-            <PriceRow
-              name={"Subtotal"}
-              amount={totalAmount}
-              className="flex items-center justify-between text-lg font-extralight italic"
-            />
+                <PriceRow
+                  name={"Delivery Fee"}
+                  amount={deliveryFee}
+                  className="flex items-center justify-between text-lg font-extralight italic"
+                />
 
-            <PriceRow
-              name={"Delivery Fee"}
-              amount={deliveryFee}
-              className="flex items-center justify-between text-lg font-extralight italic"
-            />
+                <PriceRow
+                  name={"GST 18%"}
+                  amount={Gst}
+                  className="flex items-center justify-between text-lg font-extralight italic"
+                />
 
-            <PriceRow
-              name={"GST 18%"}
-              amount={Gst}
-              className="flex items-center justify-between text-lg font-extralight italic"
-            />
+                <hr className="border-t border-dashed border-black/30 " />
 
-            <hr className="border-t border-dashed border-black/30 " />
-
-            <PriceRow
-              name={"Total"}
-              amount={total}
-              className="flex items-center justify-between text-lg"
-            />
-
-            {/* <div className="flex items-center justify-between text-lg font-extralight italic">
-              <p>Subtotal</p>
-              <span className="ml-auto flex items-center">
-                <IndianRupee size={10} />
-                <p>{totalAmount}</p>
-              </span>
-            </div>
-
-            <div className="flex items-center justify-between text-lg font-extralight italic">
-              <p>Delivery Fee</p>
-              <span className="ml-auto flex items-center italic">
-                <IndianRupee size={10} />
-                <p>{deliveryFee}</p>
-              </span>
-            </div>
-
-            <div className="flex items-center justify-between text-lg font-extralight italic">
-              <p>GST 18%</p>
-              <span className="ml-auto flex items-center">
-                <IndianRupee size={10} />
-                <p>{Gst}</p>
-              </span>
-            </div>
-
-            <hr className="border-t border-dashed border-black/30 " />
-
-            <div className="flex items-center justify-between text-lg  ">
-              <p>Total</p>
-              <span className="ml-auto flex items-center">
-                <IndianRupee size={10} />
-                <p>{total}</p>
-              </span>
-            </div> */}
+                <PriceRow
+                  name={"Total"}
+                  amount={total}
+                  className="flex items-center justify-between text-lg"
+                />
+              </>
+            ) : (
+              <p className="text-center text-gray-400 py-6">
+                Add items to cart
+              </p>
+            )}
           </div>
         </div>
 
-        <div className="max-w-lg mx-auto my-6">
-          <Button className="cursor-pointer">Proceed to payment</Button>
-        </div>
+        {cartItems.length > 0 ? (
+          <div className="max-w-lg mx-auto my-6">
+            <Button className="cursor-pointer">Proceed to payment</Button>
+          </div>
+        ) : (
+          <div className="max-w-lg mx-auto my-6">
+            <Button onClick={() => navigate("/")} className="cursor-pointer">
+              Add Items
+            </Button>
+          </div>
+        )}
       </div>
     </>
   );
