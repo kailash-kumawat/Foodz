f16745109e7fe8f949762037a04f83cc039090b7
kailash kumawat <kumawatkai369@gmail.com>
2026-04-14 13:05:25 +0530

    feat: enhance Sidebar component with close button and layout adjustments

--- frontend/src/features/home/Sidebar.jsx
+++ frontend/src/features/home/Sidebar.jsx
@@ -5,20 +5,28 @@ import {
   MapPinHouse,
   Wallet2,
   ArrowRight,
+  ArrowLeft,
   ShoppingBag,
 } from "lucide-react";
+
+// TODO: Optimise code and start building profile page
 
 function Sidebar({ isOpen, onClose }) {
   return (
     <>
       {isOpen && (
         <div
-          className={`fixed inset-0 bg-[#FA4A0C] z-40 lg:w-1/2 flex items-center transform transition-transform duration-300 `}
-          onClick={onClose}
+          className={`fixed inset-0 bg-[#FA4A0C] z-40 lg:w-1/2 flex flex-col transform transition-transform duration-300 `}
+          // onClick={onClose}
         >
+          <div className="ml-auto p-6 ">
+            <button className="cursor-pointer" onClick={() => onClose()}>
+              <ArrowLeft size={25} strokeWidth={2.5} className="text-white" />
+            </button>
+          </div>
+
           <div
-            className={`flex flex-col gap-8 text-white font-semibold w-1/2 h-auto ml-10 z-50
-        `}
+            className={`flex flex-col gap-8 text-white font-semibold w-1/2 h-auto ml-10 z-50 my-auto`}
           >
             <div className="flex gap-4 items-center">
               <UserCircle2 size={25} />
@@ -51,16 +59,18 @@ function Sidebar({ isOpen, onClose }) {
               </Link>
             </div>
             <hr className="w-5/6 ml-auto border-t border-white/30"></hr>
+          </div>
 
+          <div className="text-xl text-white font-semibold ml-10 my-auto">
             <button
-              className="mr-auto text-xl flex items-center gap-2"
+              className="flex items-center gap-2 cursor-pointer"
               onClick={() => {
                 console.log("Logout");
-                onClose();
+                // onClose();
               }}
             >
               Sign-out
-              <ArrowRight />
+              <ArrowRight size={25} strokeWidth={2.5} />
             </button>
           </div>
         </div>
